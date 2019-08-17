5d4373ea26617b6eedcd3318037981feb68854b1
banana1024 <991161108@qq.com>
2019-08-17 22:26:45 +0800

    修改代码格式错误

--- lesson04-简单回归案例实战/gd.py
+++ lesson04-简单回归案例实战/gd.py
@@ -1,13 +1,16 @@
 import numpy as np
 
+
 # y = wx + b
 def compute_error_for_line_given_points(b, w, points):
     totalError = 0
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
+        # print("x:",x, "y:",y)
         totalError += (y - (w * x + b)) ** 2
     return totalError / float(len(points))
+
 
 def step_gradient(b_current, w_current, points, learningRate):
     b_gradient = 0
@@ -16,25 +19,29 @@ def step_gradient(b_current, w_current, points, learningRate):
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
-        b_gradient += -(2/N) * (y - ((w_current * x) + b_current))
-        w_gradient += -(2/N) * x * (y - ((w_current * x) + b_current))
-    new_b = b_current - (learningRate * b_gradient)
-    new_m = w_current - (learningRate * w_gradient)
-    return [new_b, new_m]
+        b_gradient += -(2 / N) * (y - ((w_current * x) + b_current))  # 因为是所有的点的梯度累加，除以N后就不需要average了
+        w_gradient += -(2 / N) * x * (y - ((w_current * x) + b_current))
+    new_b = b_current - (learningRate * b_gradient)  # 怎么理解这一步很重要！！
+    new_w = w_current - (learningRate * w_gradient)
+    print('new_b:', new_b, 'w:', new_w)
+    return [new_b, new_w]
 
-def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
+
+def gradient_descent_runner(points, starting_b, starting_w,
+                            learning_rate, num_iterations):  # num_iterations迭代次数
     b = starting_b
-    m = starting_m
+    w = starting_w
     for i in range(num_iterations):
-        b, m = step_gradient(b, m, np.array(points), learning_rate)
-    return [b, m]
+        b, w = step_gradient(b, w, np.array(points), learning_rate)
+    return [b, w]
+
 
 def run():
     points = np.genfromtxt("data.csv", delimiter=",")
     learning_rate = 0.0001
-    initial_b = 0 # initial y-intercept guess
-    initial_m = 0 # initial slope guess
-    num_iterations = 1000
+    initial_b = 0  # initial y-intercept guess
+    initial_m = 0  # initial slope guess
+    num_iterations = 100
     print("Starting gradient descent at b = {0}, m = {1}, error = {2}"
           .format(initial_b, initial_m,
                   compute_error_for_line_given_points(initial_b, initial_m, points))
@@ -45,6 +52,7 @@ def run():
           format(num_iterations, b, m,
                  compute_error_for_line_given_points(b, m, points))
           )
+
 
 if __name__ == '__main__':
     run()
